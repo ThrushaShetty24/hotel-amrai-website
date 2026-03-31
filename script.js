fc4da805653ac05f241645ddea131487f021c309
ThrushaShetty24 <thrusha25bca@student.mes.ac.in>
2026-04-01 03:15:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function bookNow() {
+    alert("Booking Successful! We will contact you soon.");
+}
+
+function showMessage() {
+    alert("Welcome to Hotel Amrai! Enjoy your stay!");
+}
+
+function imageClick() {
+    alert("Beautiful choice!");
+}
